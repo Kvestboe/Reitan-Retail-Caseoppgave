c36d216f2dd98f29749de95af572b8b941957232
Elais Haugsbakk <elias.haugsbakk@gmail.com>
2025-10-17 23:20:38 +0200

    Oppretting av produkt-objekter fra csv fil

---
+++ src/main/handleassistent/recepies/Recipe.java
@@ -0,0 +1,4 @@
+package recepies;
+
+public class Recipe {
+}
---
+++ src/main/handleassistent/recepies/RecipeSearch.java
@@ -0,0 +1,8 @@
+package recepies;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.util.List;
+
+public class RecipeSearch {
+}
